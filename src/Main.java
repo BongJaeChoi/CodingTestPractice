a30149ea72202e069b99b64aa96935fd66f0f721
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-06 00:45:13 +0900

    2번 문자열 대문자 <-> 소문자 변환. 아스키코드가 아니라 이걸로 Character 에 static 있엇음

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public int solution(String str, char c) {
-        int answer = 0;
-
-        char[] arr = str.toCharArray();
-        for (char c1 : arr) {
-            if(c1 == c){
-                answer++;
+    public String solution(String str) {
+        StringBuilder answer = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                answer.append(Character.toUpperCase(c));
+            } else {
+                answer.append(Character.toLowerCase(c));
             }
-        }
 
-        return answer;
+        }
+        return answer.toString();
     }
 
     public static void main(String[] args) {
-
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.next().toUpperCase();
-        char c = scanner.next().toUpperCase().charAt(0);
-        System.out.print(T.solution(str, c));
+        String str = scanner.next();
+        System.out.print(T.solution(str));
     }
 }
