3baa8287bcf2c16e52ebc7560aa1ec22ab06afbe
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-06 01:25:27 +0900

    최대값 구하는 알고리즘 배움
    비교하고 크면 값 넣고 다음 element 랑 비교하고 크면 넣고

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
-    public String solution(String str) {
-        StringBuilder answer = new StringBuilder();
-        for (char c : str.toCharArray()) {
-            if (Character.isLowerCase(c)) {
-                answer.append(Character.toUpperCase(c));
-            } else {
-                answer.append(Character.toLowerCase(c));
-            }
-
-        }
-        return answer.toString();
-    }
-
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.next();
+        String str = scanner.nextLine();
         System.out.print(T.solution(str));
+    }
+
+    public String solution(String str) {
+        String answer = "";
+        String[] arr = str.split(" ");
+
+        int m = Integer.MIN_VALUE;
+
+        for (String s : arr) {
+            int length = s.length();
+            if (length > m) {
+                m = length;
+                answer = s;
+            }
+        }
+
+        return answer;
     }
 }
