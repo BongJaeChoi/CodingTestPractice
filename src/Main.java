a41a62637884627c003bad5e427961e4b461a2e3
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-06 13:24:19 +0900

    lt, rt 인덱스 서로 바꾸기 기법 도입

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,15 +11,25 @@ public class Main {
         for (int i = 0; i < input1; i++) {
             inputs[i] = scanner.next();
         }
-        for (String input : inputs) {
-            System.out.println(T.solution(input));
+        for (String input : T.solution(input1, inputs)) {
+            System.out.println(input);
         }
     }
 
-    public String solution(String str) {
-        StringBuilder answer = new StringBuilder();
-        answer.append(str);
-        answer.reverse();
-        return answer.toString();
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt]; // 0번째 인덱스 값을 마지막 인덱스 값으로 넣기
+                s[rt] = tmp; //마지막 인덱스 값을 첫번째 인덱스 값으로 넣기
+                lt++; // 반복
+                rt--;
+            }
+            answer.add(String.valueOf(s));
+        }
+        return answer;
     }
 }
