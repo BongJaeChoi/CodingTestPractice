09ed8d637584f31de729c1cb0bbfde0b65814764
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-06 15:19:29 +0900

    7번8번 회문 문자열, 유효한 팰린드롬 풀때 스트링 빌더로 입력 사본 하나 만들어서 뒤집은다음 equalsIgnoreCase 로 검사하면됨

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        System.out.println(T.solution(input));
+        String input = scanner.nextLine();
+        System.out.println(T.solution(input) ? "YES" : "NO");
     }
 
-    public String solution(String str) {
-        String answer = "";
-        char[] s = str.toCharArray();
+    public boolean solution(String str) {
+        String temp = "";
 
-        int lt = 0, rt = str.length() - 1;
-        while (lt < rt) {
-            char tmp = s[lt];
-            if (!Character.isAlphabetic(s[lt])) {
-                lt++;
-            } else if (!Character.isAlphabetic(s[rt])) {
-                rt--;
-            } else {
-                s[lt] = s[rt];
-                s[rt] = tmp;
-                lt++;
-                rt--;
+        for (int i = 0; i < str.toCharArray().length; i++) {
+            char c = str.charAt(i);
+
+            if (Character.isAlphabetic(c)) {
+                temp += c;
             }
-
         }
-        answer = String.valueOf(s);
-        return answer;
+
+        //뒤집어도 원본이랑 똑같냐
+        String reverse = new StringBuilder(temp).reverse().toString();
+
+        return temp.equalsIgnoreCase(reverse.toString());
     }
 }
