64fdf8be9a1cc80656cf0a96aa8469bbb71f0398
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-06 14:09:49 +0900

    각각 element 들을 개별로 생각해야함. 하나아니라고 반대쪽 인덱스도 건드리면 안됨

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,33 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        int input1 = scanner.nextInt();
-
-        String[] inputs = new String[input1];
-        for (int i = 0; i < input1; i++) {
-            inputs[i] = scanner.next();
-        }
-        for (String input : T.solution(input1, inputs)) {
-            System.out.println(input);
-        }
+        String input = scanner.next();
+        System.out.println(T.solution(input));
     }
 
-    public ArrayList<String> solution(int n, String[] str) {
-        ArrayList<String> answer = new ArrayList<>();
-        for (String x : str) {
-            char[] s = x.toCharArray();
-            int lt = 0, rt = x.length() - 1;
-            while (lt < rt) {
-                char tmp = s[lt];
-                s[lt] = s[rt]; // 0번째 인덱스 값을 마지막 인덱스 값으로 넣기
-                s[rt] = tmp; //마지막 인덱스 값을 첫번째 인덱스 값으로 넣기
-                lt++; // 반복
+    public String solution(String str) {
+        String answer = "";
+        char[] s = str.toCharArray();
+
+        int lt = 0, rt = str.length() - 1;
+        while (lt < rt) {
+            char tmp = s[lt];
+            if (!Character.isAlphabetic(s[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(s[rt])) {
+                rt--;
+            } else {
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
                 rt--;
             }
-            answer.add(String.valueOf(s));
+
         }
+        answer = String.valueOf(s);
         return answer;
     }
 }
