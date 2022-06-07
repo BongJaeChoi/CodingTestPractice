5caa5cdff93f01de8db7ef6768c3c43d8010dcb6
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-07 20:37:13 +0900

    슬라이드 윈도우. 완전 탐색. 앞으로 갔다가 뒤로갔다가. 포인트 변수를 최대 거리(값)큰 값으로 둔다음에 반복문을 앞뒤로 한 번 씩 한다.

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,36 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(T.solution(input));
+        String input = scanner.next();
+        char c = scanner.next().charAt(0);
+        for (int x : T.solution(input, c)) {
+            System.out.print(x + " ");
+        }
     }
 
-    public int solution(String str) {
-        String answer = "";
+    public int[] solution(String str, char c) {
+        int[] answer = new int[str.length()];
+        int p = 1000;
 
-        answer = str.replaceAll("[a-z]|[A-Z]", "");
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                p = 0;
+            } else {
+                p++;
+            }
+            answer[i] = p;
+        }
 
-        return Integer.parseInt(answer);
+        p = 1000;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == c) {
+                p = 0;
+            } else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+        return answer;
     }
 }
