b84620a127a2c7b480797d127ef6f351f34d3b95
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-07 21:45:22 +0900

    11문자열 압축 count 를 증가시키는게 맞았고, 카운트가 1 일때는 안들어가는거였음. 그리고 카운트가 초기화될때 값을 append 한다라는 생각을 해야함!

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,21 @@ public class Main {
     }
 
     public String solution(String str) {
+        str = str + " "; // 마지막 element 까지 비교하고 싶으니까 임의로 하나 추가
         String answer = "";
         int count = 1;
-        String[] x = new String[str.length()];
+
 
         for (int i = 0; i < str.length() - 1; i++) {
-            System.out.println("i :" + str.charAt(i) + " / i+1 : " + str.charAt(i + 1));
-            x[i] = String.valueOf(str.charAt(i));
-
             if (str.charAt(i) == str.charAt(i + 1)) {
                 count++;
-                x[i] = String.valueOf(str.charAt(i)) + count;
             } else {
-                count = 1;
+                if (count > 1) { // 1보다 클때만 넣어야하니까 조건식 추가됨
+
+                    // count 를 초기화시킬때 String 을 입력해야 T2,T3,T4 처럼 여러개 안들어감
+                    answer += String.valueOf(str.charAt(i)) + count;
+                    count = 1;
+                }
             }
         }
         return answer;
