a09f0904f0d49dfaabf192e7f6f8d104ca87daf2
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-07 21:29:19 +0900

    11문자열 압축. 반밖에 못 풀었다

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,23 @@ public class Main {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
-        char c = scanner.next().charAt(0);
-        for (int x : T.solution(input, c)) {
-            System.out.print(x + " ");
-        }
+        System.out.print(T.solution(input));
     }
 
-    public int[] solution(String str, char c) {
-        int[] answer = new int[str.length()];
-        int p = 1000;
+    public String solution(String str) {
+        String answer = "";
+        int count = 1;
+        String[] x = new String[str.length()];
 
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == c) {
-                p = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            System.out.println("i :" + str.charAt(i) + " / i+1 : " + str.charAt(i + 1));
+            x[i] = String.valueOf(str.charAt(i));
+
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                x[i] = String.valueOf(str.charAt(i)) + count;
             } else {
-                p++;
-            }
-            answer[i] = p;
-        }
-
-        p = 1000;
-
-        for (int i = str.length() - 1; i >= 0; i--) {
-            if (str.charAt(i) == c) {
-                p = 0;
-            } else {
-                p++;
-                answer[i] = Math.min(answer[i], p);
+                count = 1;
             }
         }
         return answer;
