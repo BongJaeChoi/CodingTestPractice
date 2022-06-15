ffc092f5e242a84c9e0ff8f33ec33220e2822f47
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-15 21:07:15 +0900

    6 뒤집은소수~~ 십만까지 무식하게 돌려버리기~~~

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,48 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int x = Integer.parseInt(scanner.nextLine());
+        String numArr = scanner.nextLine();
+        for (int s : T.solution(numArr, x)) {
+            if (s == 0 || s == 1) {
+                continue;
+            }
 
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+            System.out.print(s + " ");
         }
-
-        System.out.println(T.solution(arr));
     }
 
-    public int solution(int[] arr) {
-        int count = 0;
-        int t = 0;
+    public int[] solution(String arr, int x) {
 
-        for (int element : arr) {
-            if (element > t) {
-                count++;
-                t = element;
-            }
+        String[] strings = arr.split(" ");
+        int[] numArr = new int[x];
+
+        //자연수 뒤집기
+        for (int i = 0; i < numArr.length; i++) {
+            //첫자리부터 연속된 0은 무시
+            StringBuilder sb = new StringBuilder(strings[i].replaceAll("^00+", ""));
+
+            String s1 = sb.reverse().toString();
+            int i1 = Integer.parseInt(s1);
+            numArr[i] = i1;
         }
 
+        //뒤집은 수 중에 소수만 남기기
+        int n = 2;
+        for (int i = 0; i < numArr.length; i++) {
+            for (int j = n; j < 100000; j++) {
+                int element = numArr[i];
 
-        return count;
+                if (element / j == 1) {
+                    break;
+                }
+
+                if (element % j == 0) {
+                    numArr[i] = 0;
+                    break;
+                }
+            }
+        }
+        return numArr;
     }
 }
