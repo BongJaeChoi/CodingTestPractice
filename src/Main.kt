56cd7decb7e4aace1a6b92148fcd28348c7bf4d8
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-25 16:52:00 +0900

    중간에 빠진 숫자 찾기

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,20 @@
 fun main(args: Array<String>) {
-    println(solution(10, 1005, 30))
+    println(solution(intArrayOf(10, 6, 8, 7, 4, 5, 3, 2, 1)))
 }
 
-fun solution(X: Int, Y: Int, D: Int): Int {
+fun solution(A: IntArray): Int {
     /**
-     * 작은 개구리는 길 반대편으로 가고 싶어합니다.
-     * 개구리는 현재 위치 X에 있고 Y보다 크거나 같은 위치에 도달하려고 합니다.
-     * 작은 개구리는 항상 고정된 거리 D를 점프합니다.
-     * 최소 회수를 구하시오
+     * 배열 숫자 하나 빠졌다 찾아라~
      */
 
-    if (X == Y) {
-        return 0
+    val sortedList = A.sorted()
+
+    for (i in 1..100_000) {
+        if (!sortedList.contains(i)) {
+            return i
+        }
     }
-
-    val distance = (Y - X)
-    return Math.ceil(distance.toDouble() / D.toDouble()).toInt()
-
+    throw java.lang.RuntimeException("다 있어요~")
 }
 
 fun solution() {
