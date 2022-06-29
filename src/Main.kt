e2848b415ef4448acf6bf13edad45f18bd05847e
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-29 15:55:13 +0900

    코딩테스트 1번 감맘보 노갖다ㅏㄷ

--- src/Main.kt
+++ src/Main.kt
@@ -1,43 +1,58 @@
 fun main(args: Array<String>) {
 //    println(solution(5, intArrayOf(10, 6, 8, 4, 7, 5, 4, 4, 5, 3, 2, 1)))
-    println(
-        solution(
-            arrayOf(
-                arrayOf("yellowhat", "headgear"),
-                arrayOf("bluesunglasses", "eyewear"),
-                arrayOf("green_turban", "headgear")
-            )
-        )
-    )
-}
-
-fun solution(clothes: Array<Array<String>>): Int {
-    val map = HashMap<String, Int>()
-    for (i in clothes.indices) {
-        map[clothes[i][1]] = map.getOrDefault(clothes[i][1], 0) + 1
-    }
-
-    println(map)
-
-    var result = 1
-
-    for (i in map.keys) {
-        val i1 = map[i]
-        if (i1 != null) {
-            result *= i1
-            println(result)
-        }
-    }
-    clothes.groupBy { it[1] }.values
-        .map { it.size + 1 }
-
-
-
-
-
-    return result - 1
+//    println(solution(arrayOf("muzi","forodo","apeach","neo"), arrayOf("frodo neo","muzi forodo"),2))
+    println(solution("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"))
 }
 
 fun solution() {
 
+}
+
+fun solution(G: String): Int {
+    /**
+     * 1번 가위바위보 문제
+     * 이기면 2점 비기면 1점 지면 0점
+     */
+
+    var rResult = 0
+    var sResult = 0
+    var pResult = 0
+
+    //묵만 낼 때
+    for (c in G) {
+        val score = when (c) {
+            'R' -> 1
+            'S' -> 2
+            'P' -> 0
+            else -> throw java.lang.IllegalArgumentException("잘못된 문자열")
+        }
+        rResult += score
+    }
+
+    //찌만 낼 때
+    for (c in G) {
+        val score = when (c) {
+            'R' -> 0
+            'S' -> 1
+            'P' -> 2
+            else -> throw java.lang.IllegalArgumentException("잘못된 문자열")
+        }
+        sResult += score
+    }
+
+    //빠만 낼 때
+    for (c in G) {
+        val score = when (c) {
+            'R' -> 2
+            'S' -> 0
+            'P' -> 1
+            else -> throw java.lang.IllegalArgumentException("잘못된 문자열")
+        }
+        pResult += score
+    }
+
+    val intArr = intArrayOf(rResult, sResult, pResult)
+    intArr.sort()
+
+    return intArr.last()
 }
