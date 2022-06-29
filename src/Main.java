c500a21a118cf2b67239daca8515f5c40c93ef32
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-06-29 15:55:00 +0900

    코딩테스트 2번 제일 쉬운거 정확도 37% 나옴... 왜지?

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        System.out.println(T.solution(new String[]{"leo", "kiki", "eden"}, new String[]{"kiki", "eden"}));
+        System.out.println(T.solution(new int[]{1000, 1, 4, -1, -99, 1, -2, 2, -3, 1000}));
     }
 
     public String solution(String[] participant, String[] completion) {
 
         HashSet<String> set = new HashSet<>(Arrays.asList(completion));
+        Hashtable<String, String> hashtable = new Hashtable<>();
 
+        System.out.println(set);
         for (String s : participant) {
             if (!set.contains(s)) {
                 return s;
@@ -20,5 +23,21 @@ public class Main {
         }
 
         return "";
+    }
+
+    int solution(int[] A) {
+        /**
+         * 코드 오류찾기문제
+         * 제일 작은 숫자 구하기 -1000 ~ 1000 배열주면
+         * 그런데 왜 1번째 인덱스 부터 시작하는거임???? 이것 수정하는문제
+         */
+
+        int ans = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (ans > A[i]) {
+                ans = A[i];
+            }
+        }
+        return ans;
     }
 }
