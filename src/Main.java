276ea3ed442064abecbf51efdec427ab4a914c45
bongjaechoi <jjgod0124@gmail.com>
2022-09-05 21:57:43 +0900

    프로그래머스 스택 문제 1번

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,48 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        System.out.println(T.solution(new int[]{1000, 1, 4, -1, -99, 1, -2, 2, -3, 1000}));
+
+
+        System.out.println(T.solution(new int[]{1, 1, 3, 3, 0, 1, 1}));
+        System.out.println(Arrays.toString(Arrays.stream(T.solution(new int[]{1, 1, 3, 3, 0, 1, 1})).toArray()));
     }
 
-    public String solution(String[] participant, String[] completion) {
+    public int[] solution(int[] arr) {
 
-        HashSet<String> set = new HashSet<>(Arrays.asList(completion));
-        Hashtable<String, String> hashtable = new Hashtable<>();
+        Stack<Integer> stack = new Stack<>();
 
-        System.out.println(set);
-        for (String s : participant) {
-            if (!set.contains(s)) {
-                return s;
+        for (int i = 0; i < arr.length; i++) {
+            if (stack.empty() || (stack.peek() != arr[i])) {
+                stack.push(arr[i]);
             }
         }
 
-        return "";
-    }
+        int[] result = new int[stack.size()];
 
-    int solution(int[] A) {
-        /**
-         * 코드 오류찾기문제
-         * 제일 작은 숫자 구하기 -1000 ~ 1000 배열주면
-         * 그런데 왜 1번째 인덱스 부터 시작하는거임???? 이것 수정하는문제
-         */
-
-        int ans = 0;
-        for (int i = 0; i < A.length; i++) {
-            if (ans > A[i]) {
-                ans = A[i];
-            }
+        for (int i = 0; i < stack.size(); i++) {
+            result[i] = stack.get(i);
         }
-        return ans;
+
+        return result;
     }
+
+//    int solution(int[] A) {
+//        /**
+//         * 코드 오류찾기문제
+//         * 제일 작은 숫자 구하기 -1000 ~ 1000 배열주면
+//         * 그런데 왜 1번째 인덱스 부터 시작하는거임???? 이것 수정하는문제
+//         */
+//]
+//        int ans = A[0];
+//        for (int i = 1; i < A.length; i++) {
+//            if (ans > A[i]) {
+//                ans = A[i];
+//            }
+//        }
+//        return ans;
+//    }
+
 }
