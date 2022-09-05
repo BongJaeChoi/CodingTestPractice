32bcd55a48aceb848e597310fd629cee14cf226d
bongjaechoi <jjgod0124@gmail.com>
2022-09-05 22:30:47 +0900

    프로그래머스 스택 문제 번 괄호. 강의에도나옴

--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-
-
-        System.out.println(T.solution(new int[]{1, 1, 3, 3, 0, 1, 1}));
-        System.out.println(Arrays.toString(Arrays.stream(T.solution(new int[]{1, 1, 3, 3, 0, 1, 1})).toArray()));
+        String str = scanner.next();
+        System.out.println(T.solution(str));
     }
 
-    public int[] solution(int[] arr) {
+    public String solution(String s) {
+        String answer;
+        char[] chars = s.toCharArray();
 
-        Stack<Integer> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            if (stack.empty() || (stack.peek() != arr[i])) {
-                stack.push(arr[i]);
+        for (char aChar : chars) {
+            if (aChar == '(') {
+                stack.push(aChar);
+            } else {
+                if(stack.isEmpty()){
+                    return "NO";
+                }
+                stack.pop();
             }
         }
 
-        int[] result = new int[stack.size()];
+        answer = stack.isEmpty() ? "YES" : "NO";
 
-        for (int i = 0; i < stack.size(); i++) {
-            result[i] = stack.get(i);
-        }
-
-        return result;
+        return answer;
     }
-
-//    int solution(int[] A) {
-//        /**
-//         * 코드 오류찾기문제
-//         * 제일 작은 숫자 구하기 -1000 ~ 1000 배열주면
-//         * 그런데 왜 1번째 인덱스 부터 시작하는거임???? 이것 수정하는문제
-//         */
-//]
-//        int ans = A[0];
-//        for (int i = 1; i < A.length; i++) {
-//            if (ans > A[i]) {
-//                ans = A[i];
-//            }
-//        }
-//        return ans;
-//    }
-
 }
