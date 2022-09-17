0b0f133ad95226548013610a7fdefe6a8ffcbe43
bongjaechoi <jjgod0124@gmail.com>
2022-09-18 00:40:08 +0900

    stack 문제 2번 WHILE 문의 블록을 없앨생각은 못했음. 괄호나오면 무적권스택

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,25 @@ public class Main {
     }
 
     public String solution(String s) {
-        String answer;
-        char[] chars = s.toCharArray();
+        String answer = "";
 
         Stack<Character> stack = new Stack<>();
 
+
+        //소문자 사이 문자제거
+
+        //뒷 문자열이 괄호가 아니면 answer 에 추가
+
+        char[] chars = s.toCharArray();
+
         for (char aChar : chars) {
-            if (aChar == '(') {
+            if (aChar == ')') {
+                while (stack.pop() != '(');
+            }else {
                 stack.push(aChar);
-            } else {
-                if(stack.isEmpty()){
-                    return "NO";
-                }
-                stack.pop();
             }
         }
-
-        answer = stack.isEmpty() ? "YES" : "NO";
-
+        System.out.println(stack.toString());
         return answer;
     }
 }
