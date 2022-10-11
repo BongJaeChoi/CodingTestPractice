175f66cb4c46060bc111cdca99b0bc26c26c523a
bongjaechoi <jjgod0124@gmail.com>
2022-10-11 20:35:14 +0900

    두배열 합치기 (합쳐서 정렬해서 풀었는데 이렇게 풀면안됨.)

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,35 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[][] arr = new int[n+1][6];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= 5; j++) {
-                arr[i][j] = scanner.nextInt();
-            }
+        int i = scanner.nextInt();
+        int[] arr1 = new int[i];
+
+        for (int j = 0; j < arr1.length; j++) {
+            arr1[j] = scanner.nextInt();
         }
-        System.out.println(T.solution(n, arr));
+        int k = scanner.nextInt();
+        int[] arr2 = new int[k];
+        for (int j = 0; j < arr2.length; j++) {
+            arr2[j] = scanner.nextInt();
+        }
+
+        for (int i1 : T.solution(arr1, arr2)) {
+            System.out.print(i1 + " ");
+        }
+
     }
 
-    private int solution(int n, int[][] arr) {
-        int answer = 0; //최대값 찾아야해서 작은값으로 초기화
-        int min = Integer.MIN_VALUE;
+    private int[] solution(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length + arr2.length];
 
-        //1번학생의 카운트 변수 만들기
-        //(모두 검사)
-        //1번학생이  1학년일때 같은반 있는사람이 있으면 +
-        //1번학생이  2학년일때 같은반 있는사람이 있으면 +
-        //1번학생이  3학년일때 같은반 있는사람이 있으면 +
-        //1번학생이  4학년일때 같은반 있는사람이 있으면 +
-        //1번학생이  5학년일때 같은반 있는사람이 있으면 +
+        System.arraycopy(arr1, 0, result, 0, arr1.length);
 
-        //n번학생이 1,2,3,4,5 같은 반있는지 검사
+        System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
 
-
-        //카운트를 담을 수 있는 리스트선언
-
-        //i[0]
-        //i(행) 이 증가할때마다
-
-
-        for (int i = 1; i <= n; i++) {
-            int cnt = 0;
-            //i번 학생 카운트
-            for (int j = 1; j <= n; j++) {
-                //1학년 부터 5학년까지라서 5까지
-                for (int k = 1; k <= 5; k++) {
-                    if (arr[i][k] == arr[j][k]) { // i반 학생의 반(k)과 j학생의 반(k)
-                        cnt++;
-                        break;//???????? 중복카운팅하면 안되서 그럼 한 학생당 학년별 하나의 카운팅 이거 막혔는데 이해잘안됨..
-                        //막 숫자 11나오고 그럼. 6나오고... 5까지인데.. 이거땜에
-                    }
-                }
-            }
-            if(cnt>min){ // 같지않고 작을때만 하니까 가장 앞에 학생인게 검증됨
-                min = cnt;
-                answer = i;
-            }
-        }
-
-
-        return answer;
+        Arrays.sort(result);
+        return result;
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,140 +1,140 @@
-import java.lang.Integer.max
-import java.lang.Integer.min
-
-fun main(args: Array<String>) {
-//    println(solution(5, intArrayOf(10, 6, 8, 4, 7, 5, 4, 4, 5, 3, 2, 1)))
-//    println(solution(arrayOf("muzi","forodo","apeach","neo"), arrayOf("frodo neo","muzi forodo"),2))
-    val solution = solution(arrayOf("TR", "RT", "TR"), intArrayOf(7, 1, 3))
-}
-
-fun solution(G: String): Int {
-    /**
-     * 1번 가위바위보 문제
-     * 이기면 2점 비기면 1점 지면 0점
-     */
-
-    var rResult = 0
-    var sResult = 0
-    var pResult = 0
-
-    //묵만 낼 때
-    for (c in G) {
-        val score = when (c) {
-            'R' -> 1
-            'S' -> 2
-            'P' -> 0
-            else -> throw java.lang.IllegalArgumentException("잘못된 문자열")
-        }
-        rResult += score
-    }
-
-    //찌만 낼 때
-    for (c in G) {
-        val score = when (c) {
-            'R' -> 0
-            'S' -> 1
-            'P' -> 2
-            else -> throw java.lang.IllegalArgumentException("잘못된 문자열")
-        }
-        sResult += score
-    }
-
-    //빠만 낼 때
-    for (c in G) {
-        val score = when (c) {
-            'R' -> 2
-            'S' -> 0
-            'P' -> 1
-            else -> throw java.lang.IllegalArgumentException("잘못된 문자열")
-        }
-        pResult += score
-    }
-
-    val intArr = intArrayOf(rResult, sResult, pResult)
-    intArr.sort()
-
-    return intArr.last()
-}
-
-fun solution(survey: Array<String>, choices: IntArray): String {
-    var answer: String = ""
-//    println(survey.toList())
-//    println(choices.toList())
-
-
-    val hashMap = HashMap<Char, Int>()
-
-    hashMap['R'] = 0
-    hashMap['T'] = 0
-    hashMap['C'] = 0
-    hashMap['F'] = 0
-    hashMap['J'] = 0
-    hashMap['M'] = 0
-    hashMap['A'] = 0
-    hashMap['N'] = 0
-    val scoreArr = arrayOf(3, 2, 1, 0, 1, 2, 3)
-
-    for (i in choices.indices) {
-        //i 가 4보다 크면 두번째 인덱스
-        //아니면 첫번째 인덱스
-        //0이면 사전빠른순(아스키코드비교)
-        var key: Char
-        val score = scoreArr[choices[i] - 1]
-        key = when {
-            score == 0 -> {
-                Char(min(survey[i][0].code, survey[i][1].code))
-            }
-            choices[i] - 1 < 4 -> {
-                survey[i][0]
-            }
-            else -> {
-                survey[i][1]
-            }
-        }
-        hashMap[key] = hashMap.getOrDefault(key, hashMap[key] ?: 0) + score
-    }
-    println(hashMap)
-    val mbti = arrayOf("RT", "CF", "JM", "AN")
-    for (s in mbti) {
-        var first = s[0]
-        var second = s[1]
-
-        val firstValue = hashMap[first]!!
-        val secondValue = hashMap[second]!!
-
-        val c = when {
-            firstValue == secondValue -> {
-                Char(min(first.code, second.code))
-            }
-            firstValue > secondValue -> {
-                first
-            }
-            else -> {
-                second
-            }
-        }
-        println(c)
-        answer += c
-    }
-    println(answer)
-
-    val result = CharArray(size = 4)
-    for (index in answer.indices) {
-        when (val c = answer[index]) {
-            'T', 'R' -> {
-                result[0] = c
-            }
-            'C', 'F' -> {
-                result[1] = c
-            }
-            'J', 'M' -> {
-                result[2] = c
-            }
-            else -> {
-                result[3] = c
-            }
-        }
-    }
-
-    return result.concatToString()
-}
+//import java.lang.Integer.max
+//import java.lang.Integer.min
+//
+//fun main(args: Array<String>) {
+////    println(solution(5, intArrayOf(10, 6, 8, 4, 7, 5, 4, 4, 5, 3, 2, 1)))
+////    println(solution(arrayOf("muzi","forodo","apeach","neo"), arrayOf("frodo neo","muzi forodo"),2))
+//    val solution = solution(arrayOf("TR", "RT", "TR"), intArrayOf(7, 1, 3))
+//}
+//
+//fun solution(G: String): Int {
+//    /**
+//     * 1번 가위바위보 문제
+//     * 이기면 2점 비기면 1점 지면 0점
+//     */
+//
+//    var rResult = 0
+//    var sResult = 0
+//    var pResult = 0
+//
+//    //묵만 낼 때
+//    for (c in G) {
+//        val score = when (c) {
+//            'R' -> 1
+//            'S' -> 2
+//            'P' -> 0
+//            else -> throw java.lang.IllegalArgumentException("잘못된 문자열")
+//        }
+//        rResult += score
+//    }
+//
+//    //찌만 낼 때
+//    for (c in G) {
+//        val score = when (c) {
+//            'R' -> 0
+//            'S' -> 1
+//            'P' -> 2
+//            else -> throw java.lang.IllegalArgumentException("잘못된 문자열")
+//        }
+//        sResult += score
+//    }
+//
+//    //빠만 낼 때
+//    for (c in G) {
+//        val score = when (c) {
+//            'R' -> 2
+//            'S' -> 0
+//            'P' -> 1
+//            else -> throw java.lang.IllegalArgumentException("잘못된 문자열")
+//        }
+//        pResult += score
+//    }
+//
+//    val intArr = intArrayOf(rResult, sResult, pResult)
+//    intArr.sort()
+//
+//    return intArr.last()
+//}
+//
+//fun solution(survey: Array<String>, choices: IntArray): String {
+//    var answer: String = ""
+////    println(survey.toList())
+////    println(choices.toList())
+//
+//
+//    val hashMap = HashMap<Char, Int>()
+//
+//    hashMap['R'] = 0
+//    hashMap['T'] = 0
+//    hashMap['C'] = 0
+//    hashMap['F'] = 0
+//    hashMap['J'] = 0
+//    hashMap['M'] = 0
+//    hashMap['A'] = 0
+//    hashMap['N'] = 0
+//    val scoreArr = arrayOf(3, 2, 1, 0, 1, 2, 3)
+//
+//    for (i in choices.indices) {
+//        //i 가 4보다 크면 두번째 인덱스
+//        //아니면 첫번째 인덱스
+//        //0이면 사전빠른순(아스키코드비교)
+//        var key: Char
+//        val score = scoreArr[choices[i] - 1]
+//        key = when {
+//            score == 0 -> {
+//                Char(min(survey[i][0].code, survey[i][1].code))
+//            }
+//            choices[i] - 1 < 4 -> {
+//                survey[i][0]
+//            }
+//            else -> {
+//                survey[i][1]
+//            }
+//        }
+//        hashMap[key] = hashMap.getOrDefault(key, hashMap[key] ?: 0) + score
+//    }
+//    println(hashMap)
+//    val mbti = arrayOf("RT", "CF", "JM", "AN")
+//    for (s in mbti) {
+//        var first = s[0]
+//        var second = s[1]
+//
+//        val firstValue = hashMap[first]!!
+//        val secondValue = hashMap[second]!!
+//
+//        val c = when {
+//            firstValue == secondValue -> {
+//                Char(min(first.code, second.code))
+//            }
+//            firstValue > secondValue -> {
+//                first
+//            }
+//            else -> {
+//                second
+//            }
+//        }
+//        println(c)
+//        answer += c
+//    }
+//    println(answer)
+//
+//    val result = CharArray(size = 4)
+//    for (index in answer.indices) {
+//        when (val c = answer[index]) {
+//            'T', 'R' -> {
+//                result[0] = c
+//            }
+//            'C', 'F' -> {
+//                result[1] = c
+//            }
+//            'J', 'M' -> {
+//                result[2] = c
+//            }
+//            else -> {
+//                result[3] = c
+//            }
+//        }
+//    }
+//
+//    return result.concatToString()
+//}
