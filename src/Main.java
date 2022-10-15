64c61015d3f5e71b152484a191190b40a95dbc16
bongjaechoi <jjgod0124@gmail.com>
2022-10-15 16:16:03 +0900

    leetcode twosum sorted

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-        int i = scanner.nextInt();
-        int[] arr1 = new int[i];
+        int[] arr1 = new int[]{3, 24, 50, 79, 88, 150, 345};
+        System.out.println(Arrays.toString(T.twoSumSorted(arr1, 200)));
 
-        for (int j = 0; j < arr1.length; j++) {
-            arr1[j] = scanner.nextInt();
-        }
-        int k = scanner.nextInt();
-        int[] arr2 = new int[k];
-        for (int j = 0; j < arr2.length; j++) {
-            arr2[j] = scanner.nextInt();
-        }
+    }
 
-        for (int i1 : T.solution(i, k, arr1, arr2)) {
-            System.out.print(i1 + " ");
-        }
+    public int[] twoSumSorted(int[] numbers, int target) {
+        int[] result = new int[2];
 
+        int p1 = 0;
+        int p2 = numbers.length - 1;
+        while (p1 < p2) {
+            if (target == numbers[p1] + numbers[p2]) {
+                result[0] = p1;
+                result[1] = p2;
+                return solution(result);
+            } else if (target < numbers[p1] + numbers[p2]) {
+                // target 보다 큰값이 나오면 1번째 포인터를 이동시켜서 값을 줄인다.
+                p2--;
+            } else {
+                // target 보다 작은값이 나오면 2번째 포인터를 이동시켜서 값을 늘린다.
+                p1++;
+            }
+        }
+        return result;
+    }
+
+    public int[] solution(int[] arr) {
+        //result index 에 +1 씩 해줘야함
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] + 1;
+        }
+        return arr;
     }
 
     private ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
@@ -28,17 +44,17 @@ public class Main {
         int p1 = 0, p2 = 0; // two pointers 알고리즘
 
         while (p1 < n && p2 < m) {
-            if(a[p1] < b[p2]){
+            if (a[p1] < b[p2]) {
                 result.add(a[p1++]);
-            }else {
+            } else {
                 result.add(b[p2++]);
             }
         }
 
-        while (p1<n){ // a 배열 나머지가 있는경우
+        while (p1 < n) { // a 배열 나머지가 있는경우
             result.add(a[p1++]);
         }
-        while (p2<m){ // b 배열 나머지가 있는경우
+        while (p2 < m) { // b 배열 나머지가 있는경우
             result.add(b[p2++]);
         }
 
