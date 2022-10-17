7c7ac0e5f914d61668677a83db05e0de3f72818f
bongjaechoi <jjgod0124@gmail.com>
2022-10-17 20:20:10 +0900

    교집합 구하기 문제. 이미 풀어본 two pointer 와 비슷하지만 문제 해설이 다르면 아직 잘모르겠음

--- src/Main.java
+++ src/Main.java
@@ -4,63 +4,45 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
-//        int[] arr1 = new int[]{3, 24, 50, 79, 88, 150, 345};
-        int[] arr1 = new int[]{-1, -2, -3, -4, -5};
-        System.out.println(Arrays.toString(T.twoSum(arr1, -8)));
+        int i = scanner.nextInt();
+        int[] arr1 = new int[i];
+
+        for (int j = 0; j < arr1.length; j++) {
+            arr1[j] = scanner.nextInt();
+        }
+        int k = scanner.nextInt();
+        int[] arr2 = new int[k];
+        for (int j = 0; j < arr2.length; j++) {
+            arr2[j] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        for (Integer integer : T.solution(i, k, arr1, arr2)) {
+            System.out.print(integer + " ");
+        }
 
     }
 
-    public int[] twoSum(int[] nums, int target) {
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-
-
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            Integer k = map.get(target - num);
-            if (k != null && k != i) {
-                return new int[]{i, k};
-            }
-        }
-        throw new IllegalArgumentException("No two sum solution");
-    }
-
-    public int[] solution(int[] arr) {
-        //result index 에 +1 씩 해줘야함
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] + 1;
-        }
-        return arr;
-    }
-
+    /**
+     * 교집합 구하기
+     */
     private ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
         ArrayList<Integer> result = new ArrayList<>();
 
-        int p1 = 0, p2 = 0; // two pointers 알고리즘
+        int p1 = 0, p2 = 0;
 
-        //a[p1] < b[p2]
-        //answer.add(a[p1]); p1++
-        //else answer.add(b[p2]); p2++
-        //pointer 는 length 까지만 증가
-
-        while (p1 < n && p2 < m) {
-            if (a[p1] < b[p2]) {
-                result.add(a[p1++]);
+        while (p1 < n && p2 < m)
+            if (a[p1] < b[p2]) {//sortedArr1[p1] 은 이제 공통원소가 아니기때문에
+                p1++;
+            } else if (a[p1] == b[p2]) {
+                result.add(a[p1]);
+                p1++;
+                p2++;
             } else {
-                result.add(b[p2++]);
+                p2++;
             }
-        }
-
-        while (p1 < n) { // a 배열 나머지가 있는경우
-            result.add(a[p1++]);
-        }
-        while (p2 < m) { // b 배열 나머지가 있는경우
-            result.add(b[p2++]);
-        }
 
         return result;
     }
