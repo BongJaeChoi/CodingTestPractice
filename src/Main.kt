b14405d749fcc17eab56a16775f0168923f41814
제이 최봉재 <83569574+jay-showa@users.noreply.github.com>
2022-11-24 22:00:54 +0900

    leetcode 75 205. Isomorphic Strings

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.collections.HashMap
 import kotlin.math.ceil
 
 fun main() {
@@ -14,4 +15,27 @@ fun solution(n: Int): Int {
     val a = 7.0
     val b = n.toDouble()
     return ceil(b / a).toInt()
+}
+
+fun isIsomorphic(s: String, t: String): Boolean {
+    val map = HashMap<Char, Char>()
+    val set = HashSet<Char>()
+    for (i in s.indices) {
+        val sc = s[i]
+        val tc = t[i]
+        if (map.containsKey(sc)) { // p
+            if (map[sc] != tc) { // p 를 t 라고 매핑 했는데 아니면 false!!
+                return false
+            }
+        } else {
+            // 값이 중복됨. 매핑되지 않으므로 false
+            if (set.contains(tc)) {
+                return false
+            }
+            map[sc] = tc // p = t, a = i
+            set.add(tc) // t, i
+        }
+    }
+
+    return true
 }
