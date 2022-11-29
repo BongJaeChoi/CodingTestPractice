850c4b5d01764128546492706bc357005541da45
bongjaechoi <jjgod0124@gmail.com>
2022-11-29 21:34:13 +0900

    isSubsequence

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,7 @@ import kotlin.collections.HashMap
 import kotlin.math.ceil
 
 fun main() {
-    println(solution(29))
+    println(isSubsequence("abc", "ahbgdc"))
 }
 
 fun solution(n: Int): Int {
@@ -38,4 +38,23 @@ fun isIsomorphic(s: String, t: String): Boolean {
     }
 
     return true
+}
+
+fun isSubsequence(s: String, t: String): Boolean {
+    val sCharArray = s.toCharArray()
+    val tCharArray = t.toCharArray()
+
+    var i = 0
+    var j = 0
+
+    while (i < sCharArray.size && j < tCharArray.size) {
+        if (sCharArray[i] == tCharArray[j]) {
+            i++ // 같은 값이 있을때만 증가
+        }
+        j++
+    }
+
+
+
+    return i == s.length
 }
